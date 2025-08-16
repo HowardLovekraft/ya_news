ef6e948aef903f72f086f42a17cc8fd90a2ca54d
HowardLovekraft <117375241+HowardLovekraft@users.noreply.github.com>
2025-08-16 13:13:18 +0500

    test: refactor tests into subtests

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -15,7 +15,17 @@ class TestRoutes(TestCase):
             text='Текст'
         )
 
-    def test_home_page(self):
-        url = reverse('news:home')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None)
+        )
+
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
