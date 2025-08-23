149e04d1c5c1acc78202e2540380fd2dac9e46a4
HowardLovekraft <117375241+HowardLovekraft@users.noreply.github.com>
2025-08-23 19:37:02 +0500

    add tests of logic

--- news/tests/test_logic.py
+++ news/tests/test_logic.py
@@ -24,9 +24,71 @@ class TestCommentCreation(TestCase):
         cls.form_data = {'text': cls.COMMENT_TEXT}
 
     def test_anonymous_user_cant_create_comment(self):
-        """
-        Анонимный пользователь не может оставить комментарий.
-        """
+        """Анонимный пользователь не может оставить комментарий."""
         self.client.post(self.url, data=self.form_data)
         comments_count = Comment.objects.count()
         self.assertEqual(comments_count, 0)
+
+    def test_user_can_create_comment(self):
+        response = self.auth_client.post(self.url, data=self.form_data)
+        self.assertRedirects(response, f'{self.url}#comments')
+        comments_count = Comment.objects.count()
+        self.assertEqual(comments_count, 1)
+
+        comment = Comment.objects.get()
+        self.assertEqual(comment.text, self.COMMENT_TEXT)
+        self.assertEqual(comment.news, self.news)
+        self.assertEqual(comment.author, self.user)
+
+    def test_user_cant_use_bad_words(self):
+        bad_words_data = {
+            'text': f'Какой-то текст, {BAD_WORDS[0]}, еще текст'
+        }
+        response = self.auth_client.post(self.url, data=bad_words_data)
+        self.assertFormError(
+            response,
+            form='form',
+            field='text',
+            errors=WARNING
+        )
+        comments_count = Comment.objects.count()
+        self.assertEqual(comments_count, 0)
+
+
+class TestCommentEditDelete(TestCase):
+
+    COMMENT_TEXT = 'Текст комментария'
+    NEW_COMMENT_TEXT = 'Обновлённый комментарий'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Заголовок',
+            text='Текст'
+        )
+        news_url = reverse('news:detail', args=(cls.news.id,))
+        cls.url_to_comments = news_url + '#comments'
+
+        cls.author = User.objects.create(username='Автор комментария')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+
+        cls.reader = User.objects.create(username='Читатель')
+        cls.reader_client = Client()
+        cls.reader_client.force_login(cls.reader)
+
+        cls.comment = Comment.objects.create(
+            news=cls.news,
+            author=cls.author,
+            text=cls.COMMENT_TEXT
+        )
+        cls.edit_url = reverse('news:edit', args=(cls.comment.id,))
+        cls.delete_url = reverse('news:delete', args=(cls.comment.id,))
+        cls.form_data = {'text': cls.NEW_COMMENT_TEXT}
+
+    def test_author_can_delete_comment(self):
+        response = self.author_client.delete(self.delete_url)
+        self.assertRedirects(response, self.url_to_comments)
+
+        comment_count = Comment.objects.count()
+        self.assertEqual(comment_count, 0)
